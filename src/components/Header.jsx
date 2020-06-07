5253399c421e83b2b93517ae611fb1b4c9d84c30
thanigai1709 <babutr59@gmail.com>
2020-06-07 16:02:17 +0530

    changed components code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+import Logo from "./images/spacex-logo-black-and-white.png";
 import Sidenav from "./Sidenav";
 
-class Header extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <Sidenav />
+function Header() {
+  return (
+    <div className="header-wrp">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="header-logo">
+              <a href="#">
+                <img src={Logo} alt="logo" />
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-6 text-right">
+            <Sidenav />
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,45 @@
 import React, { Component } from "react";
-function Sidenav() {
-  return <div>side nav</div>;
+class Sidenav extends Component {
+  state = {
+    isNavActive: false
+  };
+
+  render() {
+    return (
+      <div className="side-nav-wrp">
+        <span onClick={() => this.setState({ isNavActive: true })}>
+          <i className="fas fa-bars"></i>
+        </span>
+        <div
+          className={
+            this.state.isNavActive
+              ? "off-can-nav-wrp active"
+              : "off-can-nav-wrp"
+          }
+        >
+          <div className="text-left">
+            <span
+              className="close-menu"
+              onClick={() => this.setState({ isNavActive: false })}
+            >
+              <i className="fas fa-arrow-right"></i>
+            </span>
+          </div>
+          <div className="off-can-mid-align">
+            <div className="off-can-menu">
+              <div className="menu-items">
+                <span>Menu</span>
+                <span>Menu</span>
+                <span>Menu</span>
+                <span>Menu</span>
+                <span>Menu</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Sidenav;
