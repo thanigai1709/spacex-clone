c07dd49cc806a806fa9bfb27effaac61af3ab94a
thanigai1709 <babutr59@gmail.com>
2020-06-07 16:56:25 +0530

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-6"></div>
+        <div className="col-sm-6"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
